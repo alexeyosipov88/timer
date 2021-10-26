b3ca0adc98181ca73ad13375fef0d85a9c68dd52
alexeyosipov88 <aoosipov88@gmail.com>
2021-10-26 19:53:17 +0000

    refactor

--- timer1.js
+++ timer1.js
@@ -1,13 +1,14 @@
-const args = process.argv.slice(2);
-if (args) {
-  let time;
-  for (let i = 0; i < args.length; i++) {
-    if (args[i] > 0 && typeof parseInt(args[i]) === "number") {
-      time = args[i];
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, time * 1000);
-    }
+let args = process.argv.slice(2);
+args = args.sort((a, b) => a - b)
+
+for (let i = 0; i < args.length; i++) {
+  let command = Number(args[i]) * 1000;
+  console.log(command)
+  if(command < 0 || isNaN(command)) {
+    continue;
   }
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, command)
 }
 
